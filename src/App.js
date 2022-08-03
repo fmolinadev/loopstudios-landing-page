669b84b6743f7ed97f8116e5b3e9b7ea51e80bd9
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 18:28:30 -0300

    Update App.
    Add the interactive section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Interactive from "./components/Interactive";
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Interactive />
     </>
   );
 }
