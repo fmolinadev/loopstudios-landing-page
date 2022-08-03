4aad7d5e105d23bc49be2fd121b017e84f3faaab
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 18:45:48 -0300

    Update app with OurCreations component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Interactive from "./components/Interactive";
+import OurCreations from "./components/OurCreations";
 function App() {
   return (
     <>
       <Header />
       <Hero />
       <Interactive />
+      <OurCreations />
     </>
   );
 }
