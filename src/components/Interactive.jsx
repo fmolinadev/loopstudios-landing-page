ed623e8942e9c44ca49d1afd9dd771abaac49953
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 18:28:07 -0300

    Create Interactive section and styles

---
+++ src/components/Interactive.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import User from "../images/mobile/image-interactive.jpg";
+import "../styles/Interactive.css";
+const Interactive = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <img src={User} alt="interactive user" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Interactive;
