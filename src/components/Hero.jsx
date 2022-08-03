1840eeeeb1c95a447d1bc50ea7dce0d6607fd284
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 18:15:23 -0300

    Create Hero component and Styles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Hero.css";
+const Hero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="main-hero">
+          <h1>Inmersive experiences that deliver</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
