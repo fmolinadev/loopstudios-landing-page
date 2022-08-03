bccf45d1e880aa2a7030c4d5f40a302b953d8d8f
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 18:44:48 -0300

    Create interaction component and styles

--- src/components/Interactive.jsx
+++ src/components/Interactive.jsx
@@ -4,9 +4,18 @@ import "../styles/Interactive.css";
 const Interactive = () => {
   return (
     <>
-      <section>
-        <div>
+      <section className="interactive">
+        <div className="int-image">
           <img src={User} alt="interactive user" />
+        </div>
+        <div className="int-text-container">
+          <h2>The leader in interactive VR</h2>
+          <p>
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects for some of the best companies around the globe.
+            Our award-winning creations have transformed businesses through
+            digital experiences that bind to their brand.
+          </p>
         </div>
       </section>
     </>
