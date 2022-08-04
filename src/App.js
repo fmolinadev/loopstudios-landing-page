90436657d456066d617096c60114b66e8c40d962
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 22:47:12 -0300

    Add footer.
    Mobile version: OK!

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Interactive from "./components/Interactive";
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Interactive />
       <OurCreations />
+      <Footer />
     </>
   );
 }
