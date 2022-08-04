0c6fb5ccae7750dfdad443f88de7a1da887ea691
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 22:26:06 -0300

    Import image of objet info

---
+++ src/components/common/desktop.js
@@ -0,0 +1,53 @@
+import earth from "../../images/desktop/image-deep-earth.jpg";
+import arcade from "../../images/desktop/image-night-arcade.jpg";
+import soccer from "../../images/desktop/image-soccer-team.jpg";
+import grid from "../../images/desktop/image-grid.jpg";
+import above from "../../images/desktop/image-from-above.jpg";
+import boreals from "../../images/desktop/image-pocket-borealis.jpg";
+import curiosity from "../../images/desktop/image-curiosity.jpg";
+import fisheye from "../../images/desktop/image-fisheye.jpg";
+
+const dataDesktop = [
+  {
+    id: 1,
+    cover: earth,
+    title: "Deep Earth",
+  },
+  {
+    id: 2,
+    cover: arcade,
+    title: "Night Arcade",
+  },
+  {
+    id: 3,
+    cover: soccer,
+    title: "Soccer Team",
+  },
+  {
+    id: 4,
+    cover: grid,
+    title: "The Grid",
+  },
+  {
+    id: 5,
+    cover: above,
+    title: "From Up Above Vr",
+  },
+  {
+    id: 6,
+    cover: boreals,
+    title: "Pocket Borealis",
+  },
+  {
+    id: 7,
+    cover: curiosity,
+    title: "The Curiosity",
+  },
+  {
+    id: 8,
+    cover: fisheye,
+    title: "Make it Fisheye",
+  },
+];
+
+export default dataDesktop;
---
+++ src/components/common/mobile.js
@@ -0,0 +1,53 @@
+import earth from "../../images/mobile/image-deep-earth.jpg";
+import arcade from "../../images/mobile/image-night-arcade.jpg";
+import soccer from "../../images/mobile/image-soccer-team.jpg";
+import grid from "../../images/mobile/image-grid.jpg";
+import above from "../../images/mobile/image-from-above.jpg";
+import boreals from "../../images/mobile/image-pocket-borealis.jpg";
+import curiosity from "../../images/mobile/image-curiosity.jpg";
+import fisheye from "../../images/mobile/image-fisheye.jpg";
+
+const dataMobile = [
+  {
+    id: 1,
+    cover: earth,
+    title: "Deep Earth",
+  },
+  {
+    id: 2,
+    cover: arcade,
+    title: "Night Arcade",
+  },
+  {
+    id: 3,
+    cover: soccer,
+    title: "Soccer Team",
+  },
+  {
+    id: 4,
+    cover: grid,
+    title: "The Grid",
+  },
+  {
+    id: 5,
+    cover: above,
+    title: "From Up Above Vr",
+  },
+  {
+    id: 6,
+    cover: boreals,
+    title: "Pocket Borealis",
+  },
+  {
+    id: 7,
+    cover: curiosity,
+    title: "The Curiosity",
+  },
+  {
+    id: 8,
+    cover: fisheye,
+    title: "Make it Fisheye",
+  },
+];
+
+export default dataMobile;
