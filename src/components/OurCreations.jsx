4a8b14a278213a9c83beb76b6954958f8d7e6b2f
Francisco Molina <fdm.molina@gmail.com>
2022-08-03 22:26:47 -0300

    Uptate Order Creations with render cards

--- src/components/OurCreations.jsx
+++ src/components/OurCreations.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import Card from "./Card";
+import dataMobile from "./common/mobile";
 import "../styles/OurCreations.css";
 
 const OurCreations = () => {
-  return <div>OurCreations</div>;
+  const data = dataMobile;
+
+  return (
+    <>
+      <section className="creations-mobile">
+        <h2>Our Creations</h2>
+        <div>
+          {data.map((oneCard) => {
+            console.log(oneCard);
+            return (
+              <Card
+                key={oneCard.id}
+                title={oneCard.title}
+                cover={oneCard.cover}
+              />
+            );
+          })}
+        </div>
+        <div className="container-btn">
+          <button className="btn">See All</button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default OurCreations;
