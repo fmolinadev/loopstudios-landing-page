6067235af95811f5deafe933c2eee57cfd51074c
Francisco Molina <fdm.molina@gmail.com>
2022-08-04 15:55:20 -0300

    Update app file.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Interactive from "./components/Interactive";
 import OurCreations from "./components/OurCreations";
+import OurCreationsDesktop from "./components/OurCreationsDesktop";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Interactive />
       <OurCreations />
+      <OurCreationsDesktop />
       <Footer />
     </>
   );
