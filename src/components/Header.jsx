aa42071bc848658689304b8ca4940df538cfec4a
Francisco Molina <fdm.molina@gmail.com>
2022-08-04 21:41:31 -0300

    Update nav elements and toggle fn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,16 @@ import logo from "../images/logo.svg";
 import "../styles/Header.css";
 
 const Header = () => {
+  const viewMenu = () => {
+    const menuBtn = document.querySelector(".menu-btn");
+    const navbar = document.querySelector("nav");
+
+    menuBtn.addEventListener("click", () => {
+      navbar.classList.toggle("open");
+      menuBtn.classList.toggle("rotate");
+    });
+  };
+
   return (
     <>
       <header className="header">
@@ -13,7 +23,14 @@ const Header = () => {
             aria-label="Logo de Logostudios"
           />
         </div>
-        <nav className="navbar">
+        <nav>
+          <div className="nav-logo">
+            <img
+              src={logo}
+              alt="Loopstudios logo"
+              aria-label="Logo de Logostudios"
+            />
+          </div>
           <ul>
             <li>About</li>
             <li>Carrers</li>
@@ -22,7 +39,7 @@ const Header = () => {
             <li>Support</li>
           </ul>
         </nav>
-        <div className="menu-btn">
+        <div className="menu-btn" onClick={() => viewMenu()}>
           <div className="bar bar1"></div>
           <div className="bar bar2"></div>
           <div className="bar bar3"></div>
